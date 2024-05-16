36f42bb9e09a3248f9f234e20539ad361b2208d1
Moheb619 <6moheb19@gmail.com>
2024-05-16 08:11:33 +0600

    mysql to mongodb

--- lib/auth.ts
+++ lib/auth.ts
@@ -23,36 +23,29 @@ export const authOptions: NextAuthOptions = {
         password: { label: "Password", type: "password" },
       },
       async authorize(credentials) {
-        try {
-          if (!credentials?.email || !credentials.password) {
-            return null;
-          }
-
-          const user = await db.user.findUnique({
-            where: {
-              email: String(credentials.email),
-            },
-          });
-
-          if (
-            !user ||
-            !(await bcrypt.compare(
-              String(credentials.password),
-              user.password!
-            ))
-          ) {
-            return null;
-          }
-
-          return {
-            id: user.id,
-            email: user.email,
-            name: user.name,
-            randomKey: "Hey cool",
-          };
-        } catch (err) {
-          throw new Error("Next Auth - Authorize: Authentication error");
+        if (!credentials?.email || !credentials.password) {
+          return null;
         }
+
+        const user = await db.user.findUnique({
+          where: {
+            email: String(credentials.email),
+          },
+        });
+
+        if (
+          !user ||
+          !(await bcrypt.compare(String(credentials.password), user.password!))
+        ) {
+          return null;
+        }
+
+        return {
+          id: user.id,
+          email: user.email,
+          name: user.name,
+          randomKey: "Hey cool",
+        };
       },
     }),
   ],
